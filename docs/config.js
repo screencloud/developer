ec19b39583a4ad531276fa95e3a1cf07d841d979
Michael Martin <michael@problogdesign.com>
2020-04-06 17:02:36 +0100

    GH Action to deploy docs

--- docs/config.js
+++ docs/config.js
@@ -1,7 +1,7 @@
 const config = {
   gatsby: {
-    pathPrefix: '/',
-    siteUrl: 'https://hasura.io',
+    pathPrefix: '/develop',
+    siteUrl: 'http://screencloud.github.io/',
     gaTrackingId: null,
     trailingSlash: false,
   },
