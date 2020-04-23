c2c9106231696d57190e368e92633f3bd62a3b98
Michael Martin <michael@problogdesign.com>
2020-04-23 14:44:12 +0100

    chore: add some code to test with

--- packages/apps-js-sdk/src/index.ts
+++ packages/apps-js-sdk/src/index.ts
@@ -1,1 +1,14 @@
 export const Greeter = (name: string): string => `Hello ${name}`;
+
+const onMessage = (event: MessageEvent): void => {
+  try {
+    const { origin, data } = event;
+
+    console.log(event);
+    console.log(data);
+  } catch (e) {
+    console.warn(e);
+  }
+};
+
+window.addEventListener("message", onMessage, false);
