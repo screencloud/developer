17eedd406acd4a9743577fb6ab3d5a73a09762a5
Michael Martin <michael@problogdesign.com>
2020-04-23 14:12:56 +0100

    feat: add Prettier

--- packages/apps-js-sdk/.eslintrc.js
+++ packages/apps-js-sdk/.eslintrc.js
@@ -18,6 +18,10 @@
  */
 module.exports = {
   parser: "@typescript-eslint/parser",
-  extends: ["plugin:@typescript-eslint/recommended"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+  ],
   plugins: ["@typescript-eslint"],
 };
