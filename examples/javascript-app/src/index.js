bb267289c54fcd061fb849a82dfcf3973e5d61e8
Michael Martin <michael@problogdesign.com>
2020-04-23 09:47:34 +0100

    feat: fetch chuck norris quotes from API

--- examples/javascript-app/src/index.js
+++ examples/javascript-app/src/index.js
@@ -1,3 +1,18 @@
 import "./style.css";
 
-console.log("Hello there JS world!");
+(async function () {
+  console.log("Hello there JS world!");
+  const quoteElement = document.querySelector(".quote");
+
+  try {
+    const response = await fetch("https://api.chucknorris.io/jokes/random");
+    if (response.status !== 200) {
+      throw response;
+    }
+    const data = await response.json();
+    quoteElement.textContent = data.value;
+  } catch (err) {
+    console.warn("Something went wrong loading your data.");
+    console.warn(err);
+  }
+})();
