f3cd0f3c90d60c83b9208dab825177e705d33c39
Michael Martin <michael@problogdesign.com>
2020-04-23 10:03:37 +0100

    feat: refresh quotes every x seconds

--- examples/javascript-app/src/index.js
+++ examples/javascript-app/src/index.js
@@ -1,18 +1,41 @@
 import "./style.css";
 
-(async function () {
-  console.log("Hello there JS world!");
-  const quoteElement = document.querySelector(".quote");
-
+/**
+ * Fetch a random quote.
+ * Uses the excellent, free API at: https://api.chucknorris.io/
+ *
+ * @return {string} A single quote.
+ */
+async function fetchRandomQuote() {
   try {
     const response = await fetch("https://api.chucknorris.io/jokes/random");
     if (response.status !== 200) {
       throw response;
     }
     const data = await response.json();
-    quoteElement.textContent = data.value;
+    return data.value;
   } catch (err) {
     console.warn("Something went wrong loading your data.");
     console.warn(err);
   }
-})();
+}
+
+/**
+ * Update the current on-screen quote.
+ */
+async function updateQuote() {
+  const quoteElement = document.querySelector(".quote");
+  const quoteText = await fetchRandomQuote();
+  quoteElement.textContent = quoteText;
+}
+
+/**
+ * Starts up the app.
+ */
+async function start() {
+  setInterval(updateQuote, 10000);
+  updateQuote();
+}
+
+// Kick off your app!
+start();
