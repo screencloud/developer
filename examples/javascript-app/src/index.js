bb81fbf8b7033a1d861fa33d8936876dcca7e4aa
Michael Martin <michael@problogdesign.com>
2020-04-23 10:14:27 +0100

    fix: switch to dad jokes API

--- examples/javascript-app/src/index.js
+++ examples/javascript-app/src/index.js
@@ -2,18 +2,22 @@ import "./style.css";
 
 /**
  * Fetch a random quote.
- * Uses the excellent, free API at: https://api.chucknorris.io/
+ * Uses the excellent, free API at: https://icanhazdadjoke.com/api
  *
  * @return {string} A single quote.
  */
 async function fetchRandomQuote() {
   try {
-    const response = await fetch("https://api.chucknorris.io/jokes/random");
+    const response = await fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "application/json",
+      },
+    });
     if (response.status !== 200) {
       throw response;
     }
     const data = await response.json();
-    return data.value;
+    return data.joke;
   } catch (err) {
     console.warn("Something went wrong loading your data.");
     console.warn(err);
@@ -26,6 +30,14 @@ async function fetchRandomQuote() {
 async function updateQuote() {
   const quoteElement = document.querySelector(".quote");
   const quoteText = await fetchRandomQuote();
+
+  // Not foolproof, but try to deal with the odd overly-long quote too.
+  if (quoteText.length > 180) {
+    quoteElement.classList.add("small-text");
+  } else {
+    quoteElement.classList.remove("small-text");
+  }
+
   quoteElement.textContent = quoteText;
 }
 
