dc7edb2e5d4a532a20c0d03bd892086f9f2a5732
Krishna Acondy <kc.acondy@outlook.com>
2021-03-31 21:40:53 +0100

    fix(*): make right sidebar item more visible on hover

--- docs/src/components/RightSidebar/styles.js
+++ docs/src/components/RightSidebar/styles.js
@@ -73,7 +73,7 @@ export const ListItem = styled(({ className, active, level, ...props }) => {
     position: relative;
 
     &:hover {
-      background: ${theme.colors.black};
+      background: ${theme.colors.scYellow};
     }
 
     ${props =>
